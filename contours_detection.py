b1504d4636d1a6a2df5a335919158f72c4512d4d
kevinMoreland <kmmoreland100@gmail.com>
2019-01-29 18:01:33 +0000

    stopped doing the blur and canny to save performance, seems to maintain accuracy

--- contours_detection.py
+++ contours_detection.py
@@ -16,9 +16,10 @@ mask_img = cv.inRange(hsv_img, lower_yellow, upper_yellow) #black and white
 
 #Blur image - Bilateral preserves edges but is slower than Gaussian
 #Parameters are just guesses - idk what parameters to use
-bilat_filt_img = cv.bilateralFilter(mask_img, 5, 275, 275)
+#bilat_filt_img = cv.bilateralFilter(mask_img, 5, 275, 275)
 
-edges_img = bilat_filt_img
+#b/c before we were using the edges_img, we should just use the var mask_img when we're sure this is the way to go
+edges_img = mask_img
 #gives a list of contours as outputs, fitler the list with particular shape
 
 #a sorting algorithm on the above will improve the speed, merge sort
@@ -66,7 +67,7 @@ cv.drawContours(cimg, contours_list,  -1, (158,255,0), 2)
 print(time.time() - start)
 cv.imshow('raw_img', raw_img)
 cv.imshow('mask_img', mask_img)
-cv.imshow('bilat_filt_img', bilat_filt_img)
+#cv.imshow('bilat_filt_img', bilat_filt_img)
 cv.imshow('edges_img', edges_img)
 cv.imshow('detected circles', cimg)
 
