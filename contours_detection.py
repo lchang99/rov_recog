3d50c6321886304d08316de910b1e8c5361aa365
kevinMoreland <kmmoreland100@gmail.com>
2019-01-29 17:45:30 +0000

    this seems to be a little better code, I only have one loop. I don't do canny any more, I just find contours in the blurred image. this finds less contours and seems to be more accurate. This code is also significantly faster than what I had earlier

--- contours_detection.py
+++ contours_detection.py
@@ -4,24 +4,23 @@ import time
 #TODO run against test images to make sure yellow color bounds and area min are good
 #read image
 start = time.time();
-raw_img = cv.imread('yarn.jpg', -1)
+raw_img = cv.imread('no_ball_2.jpg', -1)
 hsv_img = cv.cvtColor(raw_img, cv.COLOR_BGR2HSV) #convert to hsv
 
 #range of yellow in HSV
 lower_yellow = np.array([19, 150, 189])
 upper_yellow = np.array([30, 255, 255])
 
+#maybe we could use a binary image instead, do a different type of blur
 mask_img = cv.inRange(hsv_img, lower_yellow, upper_yellow) #black and white
 
 #Blur image - Bilateral preserves edges but is slower than Gaussian
 #Parameters are just guesses - idk what parameters to use
 bilat_filt_img = cv.bilateralFilter(mask_img, 5, 275, 275)
 
-edges_img = cv.Canny(bilat_filt_img, 100, 200)
-
+edges_img = bilat_filt_img
 #gives a list of contours as outputs, fitler the list with particular shape
-_, contours, hierarchy = cv.findContours(edges_img, cv.RETR_TREE,
-                                         cv.CHAIN_APPROX_NONE)
+
 #a sorting algorithm on the above will improve the speed, merge sort
 contours_list = []
 
@@ -33,22 +32,13 @@ lowest_error = 0.3
 #this is measuered as pixels, so we may need to change this depending on the image quality the rover captures
 area_minimum = 100
 
-for cont in contours:    
-    area = cv.contourArea(cont)
-    if(area > area_minimum):
-    	#check first if area is above the minimum so we only do this to possible balls
 
-        cv.fillPoly(edges_img, pts =[cont], color=(255,255,255))
-        #so for some reason the above doesn't tottally fill in lines, maybe unclosed contours idk, the below may be faster anyways
-
-        #This thickens the lines, and I think this closes the contours for when we call findContours again
-        cv.polylines(edges_img, cont, True, 255, 5, 8, 0)
 #now I should have far fewer contours to work with
 _, contours, hierarchy = cv.findContours(edges_img, cv.RETR_TREE, 
     cv.CHAIN_APPROX_NONE)
-
 #at this point, may not be many left in contours list because they got filled
 #so, this loop will not loop very many times
+#print(len(contours))
 for cont in contours:
     area = cv.contourArea(cont)
     
