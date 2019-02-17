7e096cc7df9ca7824cddf0218607e110c6cb57ee
lchang <lucaschang00@gmail.com>
2019-02-17 15:02:23 -0800

    livestream update

--- livestream.py
+++ livestream.py
@@ -1,19 +1,6 @@
-import cv2 as cv
+import cv2
+import time
 
-windowName = "Live Video Feed"
-cv.namedWindow(windowName)
-cap = cv.VideoCapture(-1)
+video = cv2.VideoCapture(0)
 
-if cap.isOpened():
-    ret, frame = cap.read()
-else:
-    ret = False
-
-while ret:
-    ret, fram = cap.read()
-    #output = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow(windowName, frame)
-    if cv.waitKey(1) == 27:
-        break
-cap.release()
-cv.destroyWindow(windowName)
+video.release()
